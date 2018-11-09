6411cafd5765e270364c844e424f95c5a8fef6ce
Christopher Aranda <christopher.aranda@q2ebanking.com>
2018-11-09 15:16:05 -0600

    adding file map service

--- client/q2_client.py
+++ client/q2_client.py
@@ -1,8 +1,10 @@
+from client.services.file_map_service import FileMapService
 from client.services.q2_config_service import Q2ConfigService
 
 
 class Q2Client(object):
 
     def __init__(self, **kwargs):
-        self.q2config = Q2ConfigService(**kwargs)
-        kwargs['q2token'] = self.q2config.headers.get('q2token')
+        self.q2_config = Q2ConfigService(**kwargs)
+        kwargs['q2token'] = self.q2_config.headers.get('q2token')
+        self.file_map = FileMapService(**kwargs)
---
+++ client/services/file_map_service.py
@@ -0,0 +1,14 @@
+from client.endpoints.file_map import FileMapEndpoint
+from client.services.q2_service import Q2Service
+
+
+class FileMapService(Q2Service):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def get_file_map_asset(self, asset, **file_map):
+        query_parameters = self.copy_query_parameters()
+        query_parameters.update(**file_map)
+        endpoint = FileMapEndpoint.FILE_MAP_ASSET.value.format(asset=asset)
+        return self.get(url=self.build_url(endpoint), query_parameters=query_parameters)
