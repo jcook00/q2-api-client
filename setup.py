47625bc81180ba5d417e23e7d066e19ef86456cb
Christopher Aranda <christopher.aranda@q2ebanking.com>
2018-11-09 12:19:57 -0600

    adding project files

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='Q2 Rest API Client',
+    version='0.1.0',
+    description='Q2 Rest API Client',
+    long_description=readme,
+    author='Christopher Aranda',
+    author_email='christopher.aranda@q2ebanking.com',
+    url='',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
