446baea23833d67b0403be476555de6e886a7802
Christopher Aranda <christopher.aranda@q2ebanking.com>
2018-12-07 14:32:25 -0600

    updated argument and added query parameters to call

--- q2_api_client/clients/mobile_ws/online_activity_client.py
+++ q2_api_client/clients/mobile_ws/online_activity_client.py
@@ -4,12 +4,14 @@ from q2_api_client.endpoints.mobile_ws_endpoints import OnlineActivityEndpoint
 
 class OnlineActivityClient(BaseQ2Client):
 
-    def get_online_activity_export(self, account_id):
+    def get_online_activity_export(self, activity_id, **query):
         """GET /mobilews/onlineActivityExport/{id}
 
-        :param str account_id: path parameter
+        :param str activity_id: path parameter
         :return: Response object
         :rtype: requests.Response
         """
-        endpoint = OnlineActivityEndpoint.ONLINE_ACTIVITY_EXPORT.value.format(id=account_id)
-        return self._get(url=self._build_url(endpoint))
+        endpoint = OnlineActivityEndpoint.ONLINE_ACTIVITY_EXPORT.value.format(id=activity_id)
+        query_parameters = self._copy_query_parameters()
+        query_parameters.update(**query)
+        return self._get(url=self._build_url(endpoint), query_parameters=query_parameters)
